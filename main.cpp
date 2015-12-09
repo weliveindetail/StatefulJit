2b4a23edaad13dac210ae86cfaa165b7b94ecef1
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-09 23:16:29 +0100

    Normalize includes

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <llvm/ADT/STLExtras.h>
-#include "llvm/Analysis/BasicAliasAnalysis.h"
-#include "llvm/Analysis/Passes.h"
-#include "llvm/IR/DIBuilder.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Transforms/Scalar.h"
+#include <llvm/Analysis/Passes.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/Scalar.h>
+
 #include <cctype>
 #include <cstdio>
 #include <map>
 #include <string>
 #include <vector>
+
 #include "KaleidoscopeJIT.h"
 
 using namespace llvm;
