4018bfa1552d33508327f06eec679f4a10cba3dd
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-10 21:42:52 +0100

    PrototypeAST -> TopLevelPrototypeAST: simplify codegen implementation

--- main.cpp
+++ main.cpp
@@ -549,21 +549,9 @@ Value *VarExprAST::codegen() {
 }
 
 Function *TopLevelPrototypeAST::codegen() {
-  // Make the function type:  double(double,double) etc.
-  std::vector<Type *> Doubles(Args.size(),
-    Type::getDoubleTy(getGlobalContext()));
-  FunctionType *FT =
-    FunctionType::get(Type::getDoubleTy(getGlobalContext()), Doubles, false);
-
-  Function *F =
-    Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
-
-  // Set names for all arguments.
-  unsigned Idx = 0;
-  for (auto &Arg : F->args())
-    Arg.setName(Args[Idx++]);
-
-  return F;
+  // the top-level function takes no arguments and returns a double
+  FunctionType *type = FunctionType::get(Type::getDoubleTy(getGlobalContext()), false);
+  return Function::Create(type, Function::ExternalLinkage, Name, TheModule.get());
 }
 
 Function *TopLevelExprAST::codegen() {
