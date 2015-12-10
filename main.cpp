7d8983e16f88910352eebac0297be67fe3aeb9ee
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-10 22:18:38 +0100

    No extern'd functions

--- main.cpp
+++ main.cpp
@@ -572,22 +572,6 @@ static void MainLoop() {
 }
 
 //===----------------------------------------------------------------------===//
-// "Library" functions that can be "extern'd" from user code.
-//===----------------------------------------------------------------------===//
-
-/// putchard - putchar that takes a double and returns 0.
-extern "C" double putchard(double X) {
-  fputc((char)X, stderr);
-  return 0;
-}
-
-/// printd - printf that takes a double prints it as "%f\n", returning 0.
-extern "C" double printd(double X) {
-  fprintf(stderr, "%f\n", X);
-  return 0;
-}
-
-//===----------------------------------------------------------------------===//
 // Main driver code.
 //===----------------------------------------------------------------------===//
 
