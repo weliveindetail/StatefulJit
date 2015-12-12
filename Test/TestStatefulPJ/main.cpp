2d6adcfb37ad8baf42aa71ede777d1e7c94cffc1
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-12 17:46:41 +0100

    Define custom entry point for tests to pause execution after tests and keep console open on Windows

--- Test/TestStatefulPJ/main.cpp
+++ Test/TestStatefulPJ/main.cpp
@@ -5,3 +5,11 @@ TEST(testMath, myCubeTest)
 {
   //EXPECT_EQ(1000, cubic(10));
 }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  int exitCode = RUN_ALL_TESTS();
+
+  getchar();
+  return exitCode;
+}
