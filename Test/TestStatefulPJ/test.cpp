e1af39588e2c6e44c5c1a27c77de6c1e18210baf
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-12 18:55:02 +0100

    Implement generalized hook in gtest project

--- Test/TestStatefulPJ/test.cpp
+++ Test/TestStatefulPJ/test.cpp
@@ -1,14 +1,45 @@
 #include <gtest/gtest.h>
-#include "TestStatefulPJHook.h"
+
+#include <memory>
+#include <string>
+
+#include "StatefulPJ.h"
+
+using llvm::Module;
+using llvm::orc::KaleidoscopeJIT;
+
+// ----------------------------------------------------------------------------
+
+static double Eval(KaleidoscopeJIT& jit, std::string code)
+{
+  constexpr auto nameId = "__toplevel_expr";
+
+  // prepare test
+  SetupTestInput(code);
+  getNextToken();
+
+  // parse and compile top-level function
+  auto jitSymbol = CompileTopLevelExpr(jit);
+
+  // get address and reinterpret as function pointer
+  double(*FP)() = (double(*)())(intptr_t)jitSymbol.getAddress();
+
+  // run function
+  return FP();
+}
+
+// ----------------------------------------------------------------------------
 
 TEST(testStatefulPJ, Basics)
 {
   StaticInit();
   auto jit = SetupJit();
-  EXPECT_EQ(0.0, EvaluateTopLevelExpression(*jit, "var a in a;"));
-  EXPECT_EQ(1.0, EvaluateTopLevelExpression(*jit, "var a=1 in a;"));
-  EXPECT_EQ(2.0, EvaluateTopLevelExpression(*jit, "var a=1, b=2 in b;"));
+  EXPECT_EQ(0.0, Eval(*jit, "var a in a;"));
+  EXPECT_EQ(1.0, Eval(*jit, "var a=1 in a;"));
+  EXPECT_EQ(2.0, Eval(*jit, "var a=1, b=2 in b;"));
 }
+
+// ----------------------------------------------------------------------------
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
