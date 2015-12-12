3cb9a2f02ea9a636d8b14c4206b2967a41d565f4
stefan.graenitz <stefan.graenitz@gmail.com>
2015-12-12 18:55:22 +0100

    Implement generalized hook in interactive client

--- Client/InteractiveStatefulPJ/console.cpp
+++ Client/InteractiveStatefulPJ/console.cpp
@@ -7,9 +7,7 @@
 #include <cctype>
 #include <cstdio>
 
-#include "Globals.h"
-#include "AST.h"
-#include "Parser.h"
+#include "StatefulPJ.h"
 
 using llvm::Module;
 using llvm::orc::KaleidoscopeJIT;
@@ -17,104 +15,49 @@ using llvm::legacy::FunctionPassManager;
 
 // ----------------------------------------------------------------------------
 
-static void InitializeModuleAndPassManager() {
-  delete TheModule_rawptr;
+static void HandleTopLevelExpression(KaleidoscopeJIT& jit) 
+{
+  // collect user input, parse AST and compile top-level function
+  auto jitSymbol = CompileTopLevelExpr(jit);
 
-  // Open a new module.
-  TheModule_rawptr = new Module("my cool jit", llvm::getGlobalContext());
-  TheModule_rawptr->setDataLayout(TheJIT->getTargetMachine().createDataLayout());
+  // get address and reinterpret as function pointer
+  double(*FP)() = (double(*)())(intptr_t)jitSymbol.getAddress();
 
-  // Create a new pass manager attached to it.
-  TheFPM = std::make_unique<FunctionPassManager>(TheModule_rawptr);
-
-  // Do simple "peephole" optimizations and bit-twiddling optzns.
-  TheFPM->add(llvm::createInstructionCombiningPass());
-  // Reassociate expressions.
-  TheFPM->add(llvm::createReassociatePass());
-  // Eliminate Common SubExpressions.
-  TheFPM->add(llvm::createGVNPass());
-  // Simplify the control flow graph (deleting unreachable blocks, etc).
-  TheFPM->add(llvm::createCFGSimplificationPass());
-
-  TheFPM->doInitialization();
+  // evaluate and print out result
+  fprintf(stderr, "Evaluated to %f\n", FP());
 }
 
 // ----------------------------------------------------------------------------
 
-static void HandleTopLevelExpression() {
-  InitializeModuleAndPassManager();
+/// top ::= definition | expression | ';'
+static void MainLoop() 
+{
+  KaleidoscopeJIT jit;
 
-  // Evaluate a top-level expression into an anonymous function.
-  if (auto FnAST = ParseTopLevelExpr()) {
-    if (FnAST->codegen(TheModule_rawptr, "__toplevel_expr")) {
-
-      // JIT the module containing the anonymous expression, keeping a handle so
-      // we can free it later.
-      auto H = TheJIT->addModule(std::unique_ptr<Module>(TheModule_rawptr));
-
-      // Search the JIT for the __anon_expr symbol.
-      auto ExprSymbol = TheJIT->findSymbol("__toplevel_expr");
-      assert(ExprSymbol && "Function not found");
-
-      // Get the symbol's address and cast it to the right type (takes no
-      // arguments, returns a double) so we can call it as a native function.
-      double(*FP)() = (double(*)())(intptr_t)ExprSymbol.getAddress();
-      fprintf(stderr, "Evaluated to %f\n", FP());
-
-      // Delete the anonymous expression module from the JIT.
-      TheJIT->removeModule(H);
-    }
-  }
-  else {
-    // Skip token for error recovery.
-    getNextToken();
-  }
-}
-
-// ----------------------------------------------------------------------------
-
-/// top ::= definition | external | expression | ';'
-static void MainLoop() {
-  while (1) {
-    fprintf(stderr, "ready> ");
-    switch (CurTok) {
-    case tok_eof:
-      return;
-    case ';': // ignore top-level semicolons.
-      getNextToken();
-      break;
-    default:
-      HandleTopLevelExpression();
-      break;
-    }
-  }
-}
-
-// ----------------------------------------------------------------------------
-
-int main() {
-  llvm::InitializeNativeTarget();
-  llvm::InitializeNativeTargetAsmPrinter();
-  llvm::InitializeNativeTargetAsmParser();
-
-  // Install standard binary operators.
-  // 1 is lowest precedence.
-  BinopPrecedence['='] = 2;
-  BinopPrecedence['<'] = 10;
-  BinopPrecedence['+'] = 20;
-  BinopPrecedence['-'] = 20;
-  BinopPrecedence['*'] = 40; // highest.
-
-                             // Prime the first token.
   fprintf(stderr, "ready> ");
   getNextToken();
 
-  TheJIT = std::make_unique<KaleidoscopeJIT>();
+  while (1) {
+    fprintf(stderr, "ready> ");
+    switch (CurTok)
+    {
+      case tok_eof:
+        return;
+      case ';': // ignore top-level semicolons.
+        getNextToken();
+        break;
+      default:
+        HandleTopLevelExpression(jit);
+        break;
+    }
+  }
+}
 
-  InitializeModuleAndPassManager();
+// ----------------------------------------------------------------------------
 
-  // Run the main "interpreter loop" now.
+int main()
+{
+  StaticInit();
   MainLoop();
-
   return 0;
 }
