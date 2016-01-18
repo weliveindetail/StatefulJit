4458bb3aa7bcd288a6edd02fbd77db5e14670154
stefan.graenitz <stefan.graenitz@gmail.com>
2016-01-18 21:46:22 +0100

    Add more tests

--- Clients/TestClient/test.cpp
+++ Clients/TestClient/test.cpp
@@ -31,6 +31,60 @@ static double Eval(StatefulJit& jit, std::string code)
 
 // ----------------------------------------------------------------------------
 
+TEST(LanguageFeatures, VariableInit)
+{
+  StaticInit();
+  auto jit = SetupStatefulJit();
+
+  // default init
+  EXPECT_EQ(0.0, Eval(*jit, "def double a run a;"));
+  EXPECT_EQ(0.0, Eval(*jit, "def int b run b;"));
+
+  // init with literal
+  EXPECT_EQ(1.0, Eval(*jit, "def double c=1 run c;"));
+  EXPECT_EQ(2.0, Eval(*jit, "def int d=2 run d;"));
+
+  // init with literal expression
+  EXPECT_EQ(3.0, Eval(*jit, "def double e=1+2 run e;"));
+  EXPECT_EQ(4.0, Eval(*jit, "def int f=2*2 run f;"));
+
+  // init with variable
+  EXPECT_EQ(5.0, Eval(*jit, "def double g=5, double h=g run h;"));
+  EXPECT_EQ(6.0, Eval(*jit, "def int i=6, int j=i run j;"));
+
+  // init with variable expression
+  EXPECT_EQ(7.0, Eval(*jit, "def double k=5, double l=k+2 run l;"));
+  EXPECT_EQ(8.0, Eval(*jit, "def int m=4, int n=2*m run n;"));
+}
+
+// ----------------------------------------------------------------------------
+
+TEST(LanguageFeatures, ImplicitCasts)
+{
+  StaticInit();
+  auto jit = SetupStatefulJit();
+
+  // cast global return value
+  EXPECT_EQ(0.0, Eval(*jit, "def int a run a;"));
+
+  // cast init value
+  EXPECT_EQ(1.0, Eval(*jit, "def int b=1 run b;"));
+  EXPECT_EQ(2.0, Eval(*jit, "def int c=1+1 run c;"));
+  EXPECT_EQ(3.0, Eval(*jit, "def double d=3, int e=d run e;"));
+  EXPECT_EQ(4.0, Eval(*jit, "def int f=2, double g=f*2 run g;"));
+
+  // cast operations
+  EXPECT_EQ(5.0, Eval(*jit, "def int h=2 run h+3;"));
+  EXPECT_EQ(6.0, Eval(*jit, "def double i=2, int j=3 run i*j;"));
+  EXPECT_EQ(7.0, Eval(*jit, "def int k=3, double l=4 run k+l;"));
+
+  // side effects: rounding
+  EXPECT_EQ(8.0, Eval(*jit, "def double m=8.1415, int n=m run n;"));
+  EXPECT_EQ(9.0, Eval(*jit, "def double o=4.5, int p=o*2 run p;"));
+}
+
+// ----------------------------------------------------------------------------
+
 TEST(StatefulEvaluation, SingleVariable)
 {
   StaticInit();
@@ -77,6 +131,7 @@ int main(int argc, char **argv) {
 
   // on windows don't close the console window immediately
   #ifdef _WIN32
+    fflush(stdout);
     getchar();
   #endif
 
